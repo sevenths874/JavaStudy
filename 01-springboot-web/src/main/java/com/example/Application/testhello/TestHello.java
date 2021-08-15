170a8ad4f9a82962c92b25264cb931758dfff762
lhw <2607520791@qq.com>
2021-08-15 15:04:23 +0800

    新增一行

--- 01-springboot-web/src/main/java/com/example/Application/testhello/TestHello.java
+++ 01-springboot-web/src/main/java/com/example/Application/testhello/TestHello.java
@@ -3,5 +3,6 @@ package com.example.Application.testhello;
 public class TestHello {
     public void Print() {
         System.out.println("Test...");
+        System.out.println("Test...");
     }
 }
